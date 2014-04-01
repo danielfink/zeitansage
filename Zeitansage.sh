7463fff7d769b1dea41c8abc978629f0a2d46eb3
danielfink <danielfink1986@gmail.com>
2014-04-01 15:24:50 +0200

    Update Zeitansage.sh

--- Zeitansage.sh
+++ Zeitansage.sh
@@ -8,20 +8,20 @@
 
 # "Es ist eins Uhr..." wird zu "Es ist ein Uhr...".
 if [ "`date +%k`" = "1" ]; then
- hour=ein
+ Stunden=ein
 else
- hour=`date +%k`
+ Stunden=`date +%k`
 fi
 
 # Bei vollen Stunden wird die Null nach dem Wort Uhr entfernt.
 if [ "`date +%-M`" = "0" ]; then
- minute=""
+ Minuten=""
 else
- minute=`date +%-M`
+ Minuten=`date +%-M`
 fi
 
 # eSpeak-Ausgabe wird in WAVE_Zeitansage  gespeichert.
-espeak -vde "Es ist $hour Uhr $minute." -w WAVE_Zeitansage
+espeak -vde "Es ist $Stunden Uhr $Minuten." -w WAVE_Zeitansage
 
 # OMXPlayer gibt WAVE_Zeitsage aus.
 omxplayer WAVE_Zeitansage
